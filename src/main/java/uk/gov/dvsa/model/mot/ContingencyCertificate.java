b81864b11e9342f13dde59e3eb6c26caabc77dc2
Adrian Zabrocki <A.Zabrocki@kainos.com>
2018-02-23 15:04:42 +0100

    BL-7100 Add all contingency certificates

--- src/main/java/uk/gov/dvsa/model/mot/ContingencyCertificate.java
+++ src/main/java/uk/gov/dvsa/model/mot/ContingencyCertificate.java
@@ -3,22 +3,15 @@ package uk.gov.dvsa.model.mot;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import uk.gov.dvsa.model.Document;
 
+import java.util.List;
+
 public abstract class ContingencyCertificate extends Document {
 
     @JsonProperty("InspectionAuthority")
-    private String inspectionAuthority;
+    private List<String> inspectionAuthority;
 
     @JsonProperty("Vts")
     private String vts;
-
-    public String getInspectionAuthority() {
-        return inspectionAuthority;
-    }
-
-    public ContingencyCertificate setInspectionAuthority(String inspectionAuthority) {
-        this.inspectionAuthority = inspectionAuthority;
-        return this;
-    }
 
     public String getVts() {
         return vts;
@@ -26,6 +19,15 @@ public abstract class ContingencyCertificate extends Document {
 
     public ContingencyCertificate setVts(String vts) {
         this.vts = vts;
+        return this;
+    }
+
+    public List<String> getInspectionAuthority() {
+        return inspectionAuthority;
+    }
+
+    public ContingencyCertificate setInspectionAuthority(List<String> inspectionAuthority) {
+        this.inspectionAuthority = inspectionAuthority;
         return this;
     }
 }
--- src/main/java/uk/gov/dvsa/model/mot/enums/CertificateTemplates.java
+++ src/main/java/uk/gov/dvsa/model/mot/enums/CertificateTemplates.java
@@ -2,6 +2,13 @@ package uk.gov.dvsa.model.mot.enums;
 
 public enum CertificateTemplates {
 
+    EU_CT20("MOT/EU_CT20"),
+    EU_CT30("MOT/EU_CT30"),
+
+    CT20("MOT/CT20"),
+    CT30("MOT/CT30"),
+    CT32("MOT/CT32"),
+
     VT30("MOT/VT30"),
     VT20("MOT/VT20"),
     VT20W("MOT/VT20W"),
