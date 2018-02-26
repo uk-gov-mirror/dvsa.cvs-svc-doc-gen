0559b2a93cd6a3caa9ae3e86721b8988c5109171
mrogowska <m.rogowska@kainos.com>
2018-02-26 11:18:01 +0100

    BL-7255 add caching compiled templates

--- src/main/java/uk/gov/dvsa/service/HtmlGenerator.java
+++ src/main/java/uk/gov/dvsa/service/HtmlGenerator.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import uk.gov.dvsa.exception.HtmlTemplateCompilationException;
 import uk.gov.dvsa.exception.HtmlTemplateProcessingException;
+import uk.gov.dvsa.logging.EventLogger;
 import uk.gov.dvsa.logging.EventType;
 import uk.gov.dvsa.logging.LoggingExecutor;
 import uk.gov.dvsa.model.Document;
@@ -13,14 +14,18 @@ import uk.gov.dvsa.model.mot.enums.DocumentsConfig;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class HtmlGenerator {
 
     private static final Logger logger = LogManager.getLogger(HtmlGenerator.class);
 
-    private final LoggingExecutor executor = new LoggingExecutor(logger);
+    private static Map<String, Template> templateCache = new HashMap<>();
 
+    private final LoggingExecutor executor = new LoggingExecutor(logger);
+    private final EventLogger eventLogger = new EventLogger(logger);
     private final Handlebars handlebars;
 
     public HtmlGenerator(Handlebars handlebars) {
@@ -49,8 +54,19 @@ public class HtmlGenerator {
     }
 
     private Template compileTemplate(String templateName) {
+
+        Template templateFromCache = getTemplateFromCache(templateName);
+        if (templateFromCache != null) {
+            eventLogger.logEvent(EventType.CERT_TEMPLATES_FROM_CACHE);
+            return templateFromCache;
+        }
+
         try {
-            return handlebars.compile(String.format("views/%s", templateName));
+            eventLogger.logEvent(EventType.CERT_TEMPLATES_HANDLEBARS_COMPILE);
+
+            Template compiledTemplate = handlebars.compile(String.format("views/%s", templateName));
+            addToCache(templateName, compiledTemplate);
+            return compiledTemplate;
         } catch (IOException e) {
             throw new HtmlTemplateCompilationException(e);
         }
@@ -72,5 +88,13 @@ public class HtmlGenerator {
         } catch (IOException e) {
             throw new HtmlTemplateProcessingException(e);
         }
+    }
+
+    private Template getTemplateFromCache(String templateName) {
+        return templateCache.get(templateName);
+    }
+
+    private void addToCache(String templateName, Template template) {
+        templateCache.put(templateName, template);
     }
 }
