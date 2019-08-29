53d08a7808dbdc86a1ec24906915f68e8c9b6e92
Bhavana Siddappa Hemanth <bhemanth@deloitte.co.uk>
2019-08-29 17:52:24 +0100

    Revert "Revert "Revert "Revert "CVSB-3933 Cert Template changes for HGV/TRL""""
    This reverts commit 29cf2a95d0ba8aef05d45c068df13a1a20b9daed.

---
+++ src/main/java/uk/gov/dvsa/model/cvs/CvsHgvPRS.java
@@ -0,0 +1,24 @@
+package uk.gov.dvsa.model.cvs;
+
+public class CvsHgvPRS extends CvsMotFailCertificate {
+
+    public String getTestType() {
+        return "HGV";
+    }
+
+    public String getPresentedDocumentNamePass() {
+        return "VTG5";
+    }
+
+    public String getPresentedDocumentNameFail() {
+        return "VTG30";
+    }
+
+    public String getVersionNumberPass() {
+        return "1.0";
+    }
+
+    public String getVersionNumberFail() {
+        return "1.0";
+    }
+}
---
+++ src/main/java/uk/gov/dvsa/model/cvs/VTG5A.java
@@ -0,0 +1,24 @@
+package uk.gov.dvsa.model.cvs;
+
+import uk.gov.dvsa.model.Document;
+import uk.gov.dvsa.model.cvs.certificateData.CvsMotCertificateData;
+
+public class VTG5A extends CvsMotCertificate {
+
+    public Document setData(CvsMotCertificateData data) {
+        this.data = data;
+        return this;
+    }
+
+    public String getTestType() {
+        return "Trailer";
+    }
+
+    public String getPresentedDocumentNamePass() {
+        return "VTG5A";
+    }
+
+    public String getVersionNumberPass() {
+        return "1.0";
+    }
+}
