78fca8de39cfbda128f45a8c3fb92eef01a81d5e
Kristian Carr <kristian.carr@bjss.com>
2024-03-04 12:01:35 +0000

    feat(cb2-11097): added signature to IVA30 hbs

--- src/main/java/uk/gov/dvsa/model/cvs/IVA30.java
+++ src/main/java/uk/gov/dvsa/model/cvs/IVA30.java
@@ -4,11 +4,15 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import uk.gov.dvsa.model.Document;
 import uk.gov.dvsa.model.cvs.certificateData.IvaFailCertificateData;
+import uk.gov.dvsa.model.cvs.certificateData.Signature;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class IVA30 extends Document {
     @JsonProperty("ID")
     private String id;
+
+    @JsonProperty("Signature")
+    private Signature signature;
 
     @JsonProperty("IVA_DATA")
     private IvaFailCertificateData ivaData;
@@ -30,7 +34,16 @@ public class IVA30 extends Document {
         return ivaData;
     }
 
+    public Signature getSignature() {
+        return signature;
+    }
+
     public void setIvaData(IvaFailCertificateData ivaData) {
         this.ivaData = ivaData;
+    }
+
+    public IVA30 setSignature(Signature signature) {
+        this.signature = signature;
+        return this;
     }
 }
